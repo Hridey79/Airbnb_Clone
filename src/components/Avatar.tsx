9d330bb612bc9c82b3d69e4d68249b4505acb826
HrideyVerma79 <82920883+HrideyVerma79@users.noreply.github.com>
2023-10-08 16:25:39 +0530

    navbar added

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from "next/image";
+
+// interface AvatarProps {
+//   src: string | null | undefined;
+// }
+
+const Avatar = () => {
+  return ( 
+    <Image 
+      className="rounded-full" 
+      height="30" 
+      width="30" 
+      alt="Avatar" 
+      src={'/assets/placeholder.jpg'}
+    />
+   );
+}
+ 
+export default Avatar;
